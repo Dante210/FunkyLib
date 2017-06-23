cb0edb87cfd7c8b04762203a3a43112d07320bd9
Dante210 <laurinavicius.donatas@gmail.com>
2017-06-23 17:08:07 +0300

    FuncExt

--- FuncExt.cs
+++ FuncExt.cs
@@ -6,6 +6,9 @@ namespace funkylib
     {
         public static Func<B, R> apply<A, B, R>(this Func<A, B, R> @this, A a) => b => @this(a, b);
 
+        public static Func<R> map<A, R>(this Func<A> @this, Func<A, R> func) => () => func(@this());
+        public static Func<R> map<A, R>(this Func<A> @this, Func<A, Func<R>> g) => () => g(@this())();
+
         public static Func<Reader, R> map<Reader, T, R>
             (this Func<Reader, T> f, Func<T, R> g)
             => reader => g(f(reader));
