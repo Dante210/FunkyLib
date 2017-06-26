5ed261342dfe55d75e26448f9f16029a33d58284
Dante210 <laurinavicius.donatas@gmail.com>
2017-06-26 14:58:26 +0300

    Added Try

--- F.cs
+++ F.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace funkylib
 {
     public static class F
     {
+        public static Try<A> Try<A>(Func<A> func) => () => func();
+
         public static Func<A, Func<B, R>> curry<A, B, R>(this Func<A, B, R> @this) => a => b => @this(a, b);
 
-        public static Func<A, Func<B, Func<C, R>>> curry <A, B, C, R>(this Func<A, B, C, R> @this) =>
+        public static Func<A, Func<B, Func<C, R>>> curry<A, B, C, R>(this Func<A, B, C, R> @this) =>
             a => b => c => @this(a, b, c);
     }
 }
---
+++ Try.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace funkylib
+{
+    public delegate Exceptional<A> Try<A>();
+
+    public static class TryExt
+    {
+        public static Exceptional<T> run<T>(this Try<T> @try) {
+            try {
+                return @try();
+            }
+            catch (Exception ex) {
+                return ex;
+            }
+        }
+
+        public static Try<R> map<A, R>(this Try<A> @this, Func<A, R> func)
+            => () => @this.run().fold<Exceptional<R>>(exception => exception,
+                a => func(a));
+
+        public static Try<R> flatmap<A, R>(this Try<A> @this, Func<A, Try<R>> func)
+            => () => @this.run().fold<Exceptional<R>>(exception => exception, a => func(a).run());
+
+        // LINQ
+
+        public static Try<R> Select<T, R>(this Try<T> @this, Func<T, R> func) => @this.map(func);
+
+        public static Try<RR> SelectMany<T, R, RR>
+            (this Try<T> @try, Func<T, Try<R>> bind, Func<T, R, RR> project)
+            => ()
+                => @try.run().fold(
+                    ex => ex,
+                    t => bind(t).run()
+                        .fold<Exceptional<RR>>(
+                            ex => ex,
+                            r => project(t, r))
+                );
+    }
+}
