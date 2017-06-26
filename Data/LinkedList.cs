ed512f5c2e97e085223a577393a5e6bab9f592d8
Dante210 <laurinavicius.donatas@gmail.com>
2017-06-26 23:09:58 +0300

    LinkedList

--- Data/LinkedList.cs
+++ Data/LinkedList.cs
@@ -20,107 +20,107 @@ namespace funkylib.Data
         }
 
         // for convenience
-        public T Head => Match(
+        public T Head => fold(
             () => throw new IndexOutOfRangeException(),
             (head, _) => head);
 
-        public LinkedList<T> Tail => Match(
+        public LinkedList<T> Tail => fold(
             () => throw new IndexOutOfRangeException(),
             (_, tail) => tail);
 
         // not really required, but hey...
-        public T this[int index] => Match(
+        public T this[int index] => fold(
             () => throw new IndexOutOfRangeException(),
             (head, tail) => index == 0 ? head : tail[index - 1]);
 
-        public R Match<R>(Func<R> Empty, Func<T, LinkedList<T>, R> Cons)
-            => isEmpty ? Empty() : Cons(head, tail);
+        public R fold<R>(Func<R> empty, Func<T, LinkedList<T>, R> cons)
+            => isEmpty ? empty() : cons(head, tail);
 
-        public IEnumerable<T> AsEnumerable() {
+        public IEnumerable<T> asEnumerable() {
             if (isEmpty) yield break;
             yield return head;
-            foreach (var t in tail.AsEnumerable()) yield return t;
+            foreach (var t in tail.asEnumerable()) yield return t;
         }
 
-        public override string ToString() => Match(
+        public override string ToString() => fold(
             () => "{ }",
-            (_, __) => $"{{ {string.Join(", ", AsEnumerable().map(v => v.ToString()))} }}");
+            (_, __) => $"{{ {string.Join(", ", asEnumerable().map(v => v.ToString()))} }}");
     }
 
     public static class LinkedList
     {
         // factory functions
-        public static LinkedList<T> Create<T>(T h, LinkedList<T> t) => new LinkedList<T>(h, t);
+        public static LinkedList<T> create<T>(T h, LinkedList<T> t) => new LinkedList<T>(h, t);
 
-        public static LinkedList<T> Create<T>(params T[] items)
+        public static LinkedList<T> create<T>(params T[] items)
             => items.Reverse().Aggregate(
                 new LinkedList<T>()
-                , (tail, head) => Create(head, tail));
+                , (tail, head) => create(head, tail));
 
-        public static int Length<T>(this LinkedList<T> @this) => @this.Match(
+        public static int Length<T>(this LinkedList<T> @this) => @this.fold(
             () => 0,
             (t, ts) => 1 + ts.Length());
 
-        public static LinkedList<T> Add<T>(this LinkedList<T> @this, T value)
-            => Create(value, @this);
+        public static LinkedList<T> add<T>(this LinkedList<T> @this, T value)
+            => create(value, @this);
 
-        public static LinkedList<T> Append<T>(this LinkedList<T> @this, T value)
-            => @this.Match(
-                () => Create(value, Create<T>()),
-                (head, tail) => Create(head, tail.Append(value)));
+        public static LinkedList<T> append<T>(this LinkedList<T> @this, T value)
+            => @this.fold(
+                () => create(value, create<T>()),
+                (head, tail) => create(head, tail.append(value)));
 
         public static LinkedList<A> insertAt<A>(this LinkedList<A> @this, int index, A value) =>
             index == 0
-                ? Create(value, @this)
-                : @this.Match(
+                ? create(value, @this)
+                : @this.fold(
                     () => throw new IndexOutOfRangeException(),
-                    (head, tail) => Create(head, tail.insertAt(index - 1, value)));
+                    (head, tail) => create(head, tail.insertAt(index - 1, value)));
 
         public static LinkedList<A> removeAt<A>(this LinkedList<A> @this, int index) =>
             index == 0
                 ? @this.Tail
-                : @this.Match(
+                : @this.fold(
                     () => throw new IndexOutOfRangeException(),
-                    (head, tail) => Create(head, tail.removeAt(index - 1)));
+                    (head, tail) => create(head, tail.removeAt(index - 1)));
 
         public static LinkedList<A> takeWhile<A>(this LinkedList<A> @this, Func<A, bool> predicate) =>
-            @this.Match(
+            @this.fold(
                 () => @this, (head, tail)
-                    => predicate(head) ? Create(head, tail.takeWhile(predicate)) : Create<A>());
+                    => predicate(head) ? create(head, tail.takeWhile(predicate)) : create<A>());
 
         public static LinkedList<A> takeWhile<A>(this IEnumerable<A> @this, Func<A, bool> predicate) {
-            var temp = Create<A>();
+            var temp = create<A>();
             foreach (var item in @this)
                 if (predicate(item))
-                    temp.Add(item);
+                    temp.add(item);
             return temp;
         }
 
         public static LinkedList<A> dropWhile<A>(this LinkedList<A> @this, Func<A, bool> predicate) =>
-            @this.Match(() => @this, (head, tail) => predicate(head) ? tail.dropWhile(predicate) : @this);
+            @this.fold(() => @this, (head, tail) => predicate(head) ? tail.dropWhile(predicate) : @this);
 
-        public static LinkedList<R> Map<T, R>(this LinkedList<T> @this, Func<T, R> f)
-            => @this.Match(
-                () => Create<R>(),
-                (head, tail) => Create(f(head), tail.Map(f)));
+        public static LinkedList<R> map<T, R>(this LinkedList<T> @this, Func<T, R> f)
+            => @this.fold(
+                () => create<R>(),
+                (head, tail) => create(f(head), tail.map(f)));
 
-        public static Unit ForEach<T>(this LinkedList<T> @this, Action<T> action) {
-            @this.Map(action.toFunc());
+        public static Unit forEach<T>(this LinkedList<T> @this, Action<T> action) {
+            @this.map(action.toFunc());
             return new Unit();
         }
 
-        public static LinkedList<R> Bind<T, R>(this LinkedList<T> @this, Func<T, LinkedList<R>> f)
-            => @this.Map(f).Join();
+        public static LinkedList<R> flatmap<T, R>(this LinkedList<T> @this, Func<T, LinkedList<R>> f)
+            => @this.map(f).Join();
 
-        public static LinkedList<T> Join<T>(this LinkedList<LinkedList<T>> @this) => @this.Match(
-            () => Create<T>(),
+        public static LinkedList<T> Join<T>(this LinkedList<LinkedList<T>> @this) => @this.fold(
+            () => create<T>(),
             (xs, xss) => concat(xs, Join(xss)));
 
         public static Acc Aggregate<A, Acc>(this LinkedList<A> @this, Acc acc, Func<Acc, A, Acc> func) =>
-            @this.Match(() => acc, (head, tail) => Aggregate(tail, func(acc, head), func));
+            @this.fold(() => acc, (head, tail) => Aggregate(tail, func(acc, head), func));
 
-        static LinkedList<T> concat<T>(LinkedList<T> l, LinkedList<T> r) => l.Match(
+        static LinkedList<T> concat<T>(LinkedList<T> l, LinkedList<T> r) => l.fold(
             () => r,
-            (h, t) => Create(h, concat(t, r)));
+            (h, t) => create(h, concat(t, r)));
     }
 }
--- F.cs
+++ F.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace funkylib
 {
     public static class F
     {
-        public static Func<A, Func<B, R>> curry<A, B, R>(this Func<A, B, R> @this) => a => b => @this(a, b);
+        public static R @using<TDisp, R>(
+            TDisp disposable
+            , Func<TDisp, R> f) where TDisp : IDisposable {
+            using (disposable)
+            {
+                return f(disposable);
+            }
+        }
 
-        public static Func<A, Func<B, Func<C, R>>> curry <A, B, C, R>(this Func<A, B, C, R> @this) =>
-            a => b => c => @this(a, b, c);
+        public static Func<A, Func<B, R>> curry<A, B, R>(this Func<A, B, R> @this) { return a => b => @this(a, b); }
+
+        public static Func<A, Func<B, Func<C, R>>> curry<A, B, C, R>(this Func<A, B, C, R> @this) {
+            return a => b => c => @this(a, b, c);
+        }
     }
 }
