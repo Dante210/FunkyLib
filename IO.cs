70a84f45e4af716dd1de7e6c3e13b25c11153133
Dante210 <laurinavicius.donatas@gmail.com>
2017-07-01 19:37:18 +0300

    Updated IO

--- IO.cs
+++ IO.cs
@@ -4,8 +4,22 @@ namespace funkylib
 {
     public delegate A IO<out A>();
 
-    public static class IOExts {
-        public static IO<B> flatMap<A, B>(this IO<A> io, Func<A, IO<B>> mapper) =>
-            () => mapper(io())();
+    public static class IO
+    {
+        public static readonly IO<Unit> empty = @return(() => { });
+
+        public static IO<A> @return<A>(Func<A> fn) { return new IO<A>(fn); }
+
+        public static IO<Unit> @return(Action action) {
+            return () => {
+                action();
+                return new Unit();
+            };
+        }
+    }
+
+    public static class IOExts
+    {
+        public static IO<B> flatMap<A, B>(this IO<A> io, Func<A, IO<B>> mapper) { return () => mapper(io())(); }
     }
 }
