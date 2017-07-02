89e37df09221385e3bc5f8b42154005349ec115b
Dante210 <laurinavicius.donatas@gmail.com>
2017-07-02 13:35:17 +0300

    Changed IO

--- IO.cs
+++ IO.cs
@@ -2,24 +2,45 @@
 
 namespace funkylib
 {
-    public delegate A IO<out A>();
-
     public static class IO
     {
-        public static readonly IO<Unit> empty = @return(() => { });
+        public static readonly IO<Unit> empty = a(() => { });
 
-        public static IO<A> @return<A>(Func<A> fn) { return new IO<A>(fn); }
-
-        public static IO<Unit> @return(Action action) {
-            return () => {
-                action();
-                return new Unit();
-            };
-        }
+        public static IO<A> a<A>(Func<A> fn) => new IO<A>(fn);
+        public static IO<Unit> a(Action action) => new IO<Unit>(() => {
+            action();
+            return new Unit();
+        });
     }
 
-    public static class IOExts
+    /**
+     * Allows encapsulating side effects and composing them.
+     */
+    public struct IO<A>
     {
-        public static IO<B> flatMap<A, B>(this IO<A> io, Func<A, IO<B>> mapper) { return () => mapper(io())(); }
+        readonly Func<A> fn;
+
+        public IO(Func<A> fn) { this.fn = fn; }
+
+        public IO<B> map<B>(Func<A, B> mapper) {
+            var fn = this.fn;
+            return new IO<B>(() => mapper(fn()));
+        }
+
+        public IO<B> andThen<B>(IO<B> io2) {
+            var fn = this.fn;
+            return new IO<B>(() => {
+                fn();
+                return io2.unsafeRun();
+            });
+        }
+
+        public IO<B> flatMap<B>(Func<A, IO<B>> mapper) {
+            var fn = this.fn;
+            return new IO<B>(() => mapper(fn()).unsafeRun());
+        }
+
+        /** Runs the encapsulated side effects. */
+        public A unsafeRun() => fn();
     }
 }
