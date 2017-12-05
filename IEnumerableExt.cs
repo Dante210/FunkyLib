8d028939eb5ce5acb501c104985db85807a93de6
Dante210 <laurinavicius.donatas@gmail.com>
2017-12-06 01:45:41 +0200

    exts update

--- IEnumerableExt.cs
+++ IEnumerableExt.cs
@@ -1,31 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 
-namespace funkylib
-{
-    public static class IEnumerableExt
-    {
-        public static IEnumerable<R> apply<A, R>(this IEnumerable<Func<A, R>> @this, A a) {
-            return @this.Select(func => func(a));
-        }
+namespace funkylib {
+  public static class IEnumerableExt {
+    public static IEnumerable<R> apply<A, R>(this IEnumerable<Func<A, R>> @this, A a) {
+        return @this.Select(func => func(a));
+    }
 
-        public static IEnumerable<Func<B, R>> apply<A, B, R>(this IEnumerable<Func<A, B, R>> @this, A a) => apply(
-            @this.Select(F.curry), a);
+    public static IEnumerable<Func<B, R>> apply<A, B, R>(this IEnumerable<Func<A, B, R>> @this, A a) => 
+      apply(@this.Select(F.curry), a);
 
-        public static IEnumerable<R> flatmap<A, R>(this IEnumerable<A> @this, Func<A, IEnumerable<R>> func) {
-            foreach (var item in @this)
-              foreach (var r in func(item))
-                yield return r;
-        }
+    public static IEnumerable<R> flatmap<A, R>(this IEnumerable<A> @this, Func<A, IEnumerable<R>> func) {
+        foreach (var item in @this)
+          foreach (var r in func(item))
+            yield return r;
+    }
 
-        public static IEnumerable<R> map<A, R>(this IEnumerable<A> @this, Func<A, R> func) {
-            foreach (var item in @this) {
-                yield return func(item);
-            }
+    public static IEnumerable<R> map<A, R>(this IEnumerable<A> @this, Func<A, R> func) {
+        foreach (var item in @this) {
+            yield return func(item);
         }
-      public static IEnumerable<T> append<T>(this IEnumerable<T> source
-        , params T[] ts) => source.Concat(ts);
-    
+    }
+
+    public static IEnumerable<T> append<T>(this IEnumerable<T> source, params T[] ts) => source.Concat(ts);
+
+    public static IEnumerable<A> flatten<A>(this IEnumerable<IEnumerable<A>> enumerable) {
+      var temp = new List<A>();
+      foreach (var variable in enumerable) {
+        temp.AddRange(variable);
+      }
+      return temp;
+    }
+
+    public static IEnumerable<A> flatten<A>(this IEnumerable<ImmutableArray<A>> enumerable) {
+      var temp = new List<A>();
+      foreach (var variable in enumerable) {
+        temp.AddRange(variable);
+      }
+      return temp;
+    }
   }
 }
