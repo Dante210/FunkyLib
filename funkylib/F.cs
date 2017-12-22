d495ad5ae3c1db5969b9298600c4a22883f82488
Dante210 <laurinavicius.donatas@gmail.com>
2017-12-22 09:04:56 +0200

    Moved to folder

---
+++ funkylib/F.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace funkylib
+{
+    public static class F
+    {
+        public static Try<A> Try<A>(Func<A> func) => () => func();
+        
+        public static R @using<TDisp, R>(
+            TDisp disposable
+            , Func<TDisp, R> f) where TDisp : IDisposable {
+            using (disposable)
+            {
+                return f(disposable);
+            }
+        }
+
+        public static Func<A, Func<B, R>> curry<A, B, R>(this Func<A, B, R> @this) { return a => b => @this(a, b); }
+
+        public static Func<A, Func<B, Func<C, R>>> curry<A, B, C, R>(this Func<A, B, C, R> @this) {
+            return a => b => c => @this(a, b, c);
+        }
+    }
+}
---
+++ funkylib/FuncExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace funkylib
+{
+    public static class FuncExt
+    {
+        public static Func<B, R> apply<A, B, R>(this Func<A, B, R> @this, A a) => b => @this(a, b);
+
+        public static Func<R> map<A, R>(this Func<A> @this, Func<A, R> func) => () => func(@this());
+        public static Func<R> map<A, R>(this Func<A> @this, Func<A, Func<R>> g) => () => g(@this())();
+
+        public static Func<Reader, R> map<Reader, T, R>
+            (this Func<Reader, T> f, Func<T, R> g)
+            => reader => g(f(reader));
+
+        public static Func<Reader, R> Select<Reader, B, R>(this Func<Reader, B> func, Func<B, R> g) => func.map(g);
+
+        public static Func<Reader, P> SelectMany<Reader, T, R, P>
+            (this Func<Reader, T> f, Func<T, Func<Reader, R>> bind, Func<T, R, P> project)
+            => reader => {
+                var t = f(reader);
+                var r = bind(t)(reader);
+                return project(t, r);
+            };
+    }
+}
